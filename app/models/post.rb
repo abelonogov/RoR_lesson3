fae6b0671532cc6a07764f31b1ada3ae0860feeb
abelonogov <acebel@mail.ru>
2014-11-12 01:03:13 +0200

    Fixed validation of uniqueness.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
   validates :body, presence: true
-  validates :title, presence: true, uniqueness: true, length: {minimum: 5, maximum: 100}
+  validates :title, presence: true, uniqueness: {scope: :user_id}, length: {minimum: 5, maximum: 100}
   belongs_to :user
 
 end
